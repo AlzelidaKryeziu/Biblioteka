b78a7ba3e3c8185fcb41044f8ed1611f723d4eaa
ardialiqkaj <ardi.aliqkaj@gmail.com>
2021-03-28 04:34:37 +0200

    design

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12">
-                <iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp=42.647522026323195~21.152810841636075&lvl=17&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no"></iframe>            </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.6700195047615!2d21.15093531533199!3d42.64715497916858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549e8d5d607f25%3A0xa31dd05b21bd09de!2sUniversiteti%20p%C3%ABr%20Biznes%20dhe%20Teknologji!5e0!3m2!1sen!2s!4v1616898746472!5m2!1sen!2s" width="500" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>            </div>
             <div class="col-lg-6 col-md-6 col-sm-12">
                 <form @submit.prevent="onSubmit">
                     <div class="form-group">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="home">
     <h4>Online Library</h4>
-    <a>This website serves as an online library where users can view the different available books</a><br>
+    <a>This website serves as an online library where users can view the different available books.</a>
+    <b-container fluid class="p-4 bg-dark">
+      <b-row>
+        <b-col>
+          <b-img class="foto1" thumbnail fluid src="https://i.pinimg.com/originals/94/d8/09/94d80913693192df5f2c79b0febb85d0.jpg"></b-img>
+          <b-dd-text class="teksti">Ana Karenina</b-dd-text>
+        </b-col>
+        <b-col>
+          <b-img class="foto2" thumbnail fluid src="https://images.booksense.com/images/007/315/9780062315007.jpg"></b-img>
+          <b-dd-text class="teksti">Alkemisti</b-dd-text>
+        </b-col>
+        <b-col>
+          <b-img class="foto3" thumbnail fluid src="https://m.media-amazon.com/images/I/41cQTFV5hPL.jpg"></b-img>
+          <b-dd-text class="teksti">Krim dhe Ndeshkim</b-dd-text>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -16,3 +32,17 @@ export default {
   }
 }
 </script>
+<style>
+.foto1{
+  width: 42%;
+}
+.foto2{
+  width: 40%;
+}
+.foto3{
+  width: 38%;
+}
+.teksti{
+  color: white;
+}
+</style>
